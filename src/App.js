71028f62cecced996aa527d1a16bd927fd8b60a1
yieldjessyield <jess.harrelson0707@gmail.com>
2017-03-21 23:10:06 -0400

    installed superagent and made first call to api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import AllPeople from './components/AllPeople.js'
 import './App.css';
 
 class App extends Component {
@@ -6,11 +7,14 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to Spotify People Maker</h2>
+          <img alt="" src="https://gallery.mailchimp.com/4670f46c8008a4a44f34600a2/images/9917ba68-194b-4ce7-a09a-b7cc53716f02.png" width="200"/>
+          <h2>Welcome to People Maker</h2>
         </div>
+          < AllPeople />
       </div>
     );
   }
 }
 
+// class="mcnImage"
 export default App;
---
+++ src/components/AllPeople.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ajax from 'superagent';
+// import $ from 'jquery'
+// import axios from 'axios';
+
+export default class AllPeople extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      people: []
+    };
+  }
+
+  componentWillMount() {
+    ajax.get('http://localhost:3001/people')
+        .withCredentials()
+        .end((error, response) => {
+            if (!error && response) {
+              debugger
+              // the response.body is a bit of SuperAgent magic
+              this.setState({ people: response.body.people });
+            } else {
+              debugger
+                console.log('There was an error fetching from GitHub', error);
+            }
+        }
+    );
+  }
+  // componentDidMount() {
+  //   request
+  //     .get('http://localhost:3001/people.json')
+  //     .end(function(err, res){
+  //       if (err || !res.ok) {
+  //         debugger
+  //         alert('Oh no! error');
+  //       } else {
+  //         debugger
+  //         alert('yay got ' + JSON.stringify(res.body));
+  //       }
+  //     });
+  //   // fetch('http://localhost:3001/people.json')  
+  //   //   .then(function(response) {
+  //   //     debugger
+  //   //     return response.json()
+  //   //   })
+  // }
+
+  render() {
+    return (
+      <div>
+        <h1>All The People</h1>
+        <ul>
+          {this.state.people.map(person =>
+            <li key={person.id}>{person.name} + {person.favoriteCity}</li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
+
+
+
+// ReactDOM.render(
+//   <AllPeople subreddit="reactjs"/>,
+//   document.getElementById('root')
+// );
